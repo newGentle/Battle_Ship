c034fee7969ba772b91e3c989d7e745259612bfa
Akbar <abdullaev.akbar@gmail.com>
2022-08-22 17:37:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,69 @@
 from random import randint
 
-coord = ['A', 'B', 'C', 'D', 'E', 'F']
+letters = 'ABCDEF'
+class Dot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-board = [{coord[j] + str(i + 1): '░'} for i in range(6) for j in range(6)]
-game = board.copy()
-def make_board(game):
-    print(f'  A B C D E F')
-    cnt = 0
-    for i in range(6):
-        print(i + 1,
-              game[cnt].get('A' + str(i + 1)),
-              game[cnt + 1].get('B' + str(i+1)),
-              game[cnt + 2].get('C' + str(i + 1)),
-              game[cnt + 3].get('D' + str(i + 1)),
-              game[cnt + 4].get('E' + str(i + 1)),
-              game[cnt + 5].get('F' + str(i + 1)))
-        for j in range(6):
-            cnt += 1
-        # print(cnt)
-# print('■')
-# for i in range(6):
-#      print(*board[i], end=' ')
-# print(board)
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
-print(make_board(game))
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
 
-player = input(': ')
-for i in game:
-    i[player] = 'X'
 
-print(make_board(game))
+class Board:
+
+    def __init__(self):
+        self.field = [['~'] * 6 for i in range(6)]
+
+    def __str__(self):
+        print(f'  1 2 3 4 5 6')
+        for i, j in enumerate(self.field):
+            print(f'{letters[i]} ' + ' '.join(j))
+        return ''
+
+
+class Ship:
+    def __init__(self):
+        pass
+
+    def rand_ship(self, rnd_coords):
+        board = Board()
+        self.x = rnd_coords.x
+        self.y = rnd_coords.y
+        if 0 <= self.x <= 5 and 0 <= self.y <= 5:
+            return self.x, self.y
+        return False
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def rand_coord(self):
+        # board = Board()
+        # board.field[randint(0, 5)][randint(0, 5)] = '■'
+        rnd_coord = Dot(randint(0, 5), randint(0, 5))
+        ship = Ship()
+        return ship.rand_ship(rnd_coord)
+
+    def play(self):
+        pl = input(': ')
+        if pl[0] in letters:
+            x = int(letters.index(pl[0]))
+            y = int(pl[1])
+            b.field[x][y - 1] = '■'
+        return ''
+
+# b = Board()
+# b.field[4][5] = '■'
+# print(b.make_board())
+#
+# a = Game()
+# print(a.play())
+# print(b)
+
+b = Game()
+print(b.rand_coord())
