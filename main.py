5700330142dd827546307e37a628552259013b1c
Akbar <abdullaev.akbar@gmail.com>
2022-08-22 22:37:33 +0500

    cont...

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Board:
     def __init__(self):
         self.field = [['~'] * 6 for i in range(6)]
 
-    def __str__(self):
+    def __str__(self): # создать список для доски для ретурна
         print(f'  1 2 3 4 5 6')
         for i, j in enumerate(self.field):
             print(f'{letters[i]} ' + ' '.join(j))
@@ -26,16 +26,27 @@ class Board:
 
 
 class Ship:
+    coords_of_ships = []
     def __init__(self):
         pass
 
-    def rand_ship(self, rnd_coords):
-        board = Board()
+    def rand_ship(self, rnd_coords, ship, orientation):
+        # board = Board()
         self.x = rnd_coords.x
         self.y = rnd_coords.y
-        if 0 <= self.x <= 5 and 0 <= self.y <= 5:
-            return self.x, self.y
-        return False
+        for i in range(ship):
+            if i == 0:
+                self.coords_of_ships.append([self.x, self.y])
+                continue
+            if 0 <= self.x <= 5 and 0 <= self.y <= 5:
+                if orientation:
+                    self.x += 1
+                else:
+                    self.y += 1
+                self.coords_of_ships.append([self.x, self.y])
+        return self.coords_of_ships
+
+        # return False
 
 
 class Game:
@@ -43,11 +54,18 @@ class Game:
         pass
 
     def rand_coord(self):
-        # board = Board()
+        board = Board()
         # board.field[randint(0, 5)][randint(0, 5)] = '■'
-        rnd_coord = Dot(randint(0, 5), randint(0, 5))
         ship = Ship()
-        return ship.rand_ship(rnd_coord)
+        _ships_ = [3, 2, 2, 1, 1, 1]
+        for _ship_ in _ships_:
+            rnd_coord = Dot(randint(0, 5), randint(0, 5))
+            orientation = randint(0, 1)
+            ship.rand_ship(rnd_coord, _ship_, orientation)
+        for crd in ship.coords_of_ships:
+            board.field[crd[0]][crd[1]] = '■'
+        return board
+
 
     def play(self):
         pl = input(': ')
